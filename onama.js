efb3b58aadf2c13a7d832001a331c134d1015adb
Saban2412 <emir.sabanovic.23@size.ba>
2024-05-12 18:08:46 +0200

    TRECI ESPRESO

---
+++ onama.js
@@ -0,0 +1,19 @@
+document.getElementById('scroll1').addEventListener('click', function() {
+  var contentElement = document.getElementById('slika1');
+  contentElement.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scroll2').addEventListener('click', function() {
+  var contentElement = document.getElementById('slika2');
+  contentElement.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scroll3').addEventListener('click', function() {
+  var contentElement = document.getElementById('slika3');
+  contentElement.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scroll4').addEventListener('click', function() {
+  var contentElement = document.getElementById('slika4');
+  contentElement.scrollIntoView({ behavior: 'smooth' });
+});
